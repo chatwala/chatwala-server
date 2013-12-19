d03214e2341afda20f459a5b6d47b683dbbbe840
K <mustkre8@gmail.com>
2013-12-19 09:48:53 -0500

    fixed issue with chunked upload being corrupted
    Signed-off-by: K <mustkre8@gmail.com>

--- routes/messages.js
+++ routes/messages.js
@@ -11,9 +11,17 @@ var format = require('util').format;
 var account = "chatwala";
 var access_key = "mEJKFMneQXAaYh3lbUKaoWUeMZR9t+5uqJbvcaRJ0+KRbiZiNaaUg1t3jUsM5UWMf8RhEQXCo5BzcCOANZjkEA==";
 var host = "chatwala";
-
 var blobService = null;
+var NO_BODY = "files not found";
+var NO_FILES = "body not found";
+var mongo_url = "mongodb://chatwala_mongo:CbvTA5.gkm.N9DJhYtWgKy1HRQZRGB_4mAftidt4wkA-@ds035787.mongolab.com:35787/chatwala_mongo";
 
+
+
+/**
+ Lazy Creation of Blob Service
+
+**/
 function getBlobService()
 {
 	if(blobService == null)
@@ -21,10 +29,11 @@ function getBlobService()
 		blobService = azure.createBlobService(account,access_key);
 		blobService.createContainerIfNotExists("messages", function(error){
 		    if(!error){
-		        // Container exists and is private
 				console.log("messages table ready!");
-		    }
-
+		    }else{
+				console.log("failed to connect to blob service!");
+				blobService = null;
+			}
 		});
 	}
 	return blobService;
@@ -34,11 +43,6 @@ function getBlobService()
 
 
 
-var NO_BODY = "files not found";
-var NO_FILES = "body not found";
-
-var mongo_url = "mongodb://chatwala_mongo:CbvTA5.gkm.N9DJhYtWgKy1HRQZRGB_4mAftidt4wkA-@ds035787.mongolab.com:35787/chatwala_mongo";
-var server_hostname;
 
 
 
@@ -46,8 +50,6 @@ function getUserMessages( req, res )
 {
 	console.log("fetching messages");
 	var user_id = req.params.user_id;
-	
-	
 	MongoClient.connect(mongo_url, function(err, db)
 	{
 		if(err)throw err;
@@ -55,7 +57,7 @@ function getUserMessages( req, res )
 		collection.find({"user_id":user_id}).toArray(function(err,objects){
 			if(!err)
 			{
-				console.log(objects);
+				console.log(JSON.stringify(objects,undefined,4));
 				if(objects.length)
 				{
 					var user = objects[0];
@@ -65,40 +67,31 @@ function getUserMessages( req, res )
 					// user not found
 					res.send(500,{"status":"user not found"});
 				}
-				
-				
 			}else{
 				res.send(500,err);
 			}
 			db.close();
 		});
 	});
-	
-	
-	
-	
-	// getBlobService().listBlobs("messages", function(error, blobs){
-	// 	    if(!error){
-	// 	        for(var index in blobs){
-	// 	            console.log(blobs[index].name);
-	// 				results.messages.push(blobs[index].name);
-	// 	        }
-	// 			res.send(200,results);
-	// 	    }
-	// 	});
 }
 
+
+/**
+	Endpoint Handler for retrieving message file
+**/
 function getMessage( req, res )
 {
 	var message_id = req.params.message_id;
+	
 	console.log("fetching path for message_id:",message_id);
-	var temp_file_name = GUIDUtil.GUID();
-	var newPath = __dirname + "/temp/"+temp_file_name;
+	
+	var newPath = createTempFilePath();
+	
 	getBlobService().getBlobToFile("messages", message_id, newPath, function(error){
 		if(!error)
 		{
-
-			res.sendfile( newPath, function(err){
+			console.log("send file: ",newPath);
+			res.sendfile(newPath, function(err){
 				if(err) throw err;
 				fs.unlink(newPath, function (err) {
 				  if (err) throw err;
@@ -109,6 +102,7 @@ function getMessage( req, res )
 			
 		}else{
 			console.log("failed to retrieve file");
+			res.send(200,{"status":"message not found", "message_id":message_id});
 		}
 	});
 }
@@ -121,19 +115,16 @@ function submitMessageMetadata( req, res )
 	{
 		var recipient_id = req.body.recipient_id;
 		var sender_id = req.body.sender_id;
-		var message_id = GUIDUtil.GUID();
-		var new_dir = __dirname + "/uploads/"+message_id;
-		fs.mkdir(new_dir, function(err){
+		console.log("recieved metadata!");
+		
+		var message_metadata =  req.body;
+		message_metadata.message_id = GUIDUtil.GUID();
+		
+		saveOutGoingMessage(message_metadata, function(err){
 			if(err)throw err;
-			console.log("recieved metadata!");
-			saveOutGoingMessage(req.body,message_id, function(err){
-				if(err)throw err;
-				var results = {status:"OK", message_id:message_id, url: ("chatwala://message/" + message_id)};
-				console.log("sending response: ",results);
-				res.send(200, results);
-			});
-			
-			
+			var results = {status:"OK", message_id:message_metadata.message_id, url: ("chatwala://message/" + message_metadata.message_id)};
+			console.log("sending response: ",results);
+			res.send(200, results);
 		});
 	}else{
 		console.log(NO_BODY);
@@ -143,7 +134,7 @@ function submitMessageMetadata( req, res )
 
 
 
-function saveOutGoingMessage( message_metadata, message_id, callback )
+function saveOutGoingMessage( message_metadata, callback )
 {
 	MongoClient.connect(mongo_url, function(err, db)
 	{
@@ -172,49 +163,64 @@ function saveOutGoingMessage( message_metadata, message_id, callback )
 				db.close();
 			});
 		}
-		
-		
-		
-	
 	});
 }
 
-
+/**
+ Endpoint Handler for Chatwala File
+**/
 function uploadMessage( req, res )
 {
+	
+	
+	// get message_id parameter
 	var message_id = req.params.message_id;
-	var recipient_id = req.body.recipient_id;
-	var sender_id = req.body.sender_id;
-	var boddarr = [];
-	var message;
-	console.log("BODY",req.body);
+	// create a temp file
+	var tempFilePath = createTempFilePath();
+	var file = fs.createWriteStream(tempFilePath);
+	
+
+	var fileSize = req.headers['content-length'];
+	var uploadedSize = 0;
 	
 	console.log("storing message blob with ID:",message_id);
-	
+
+	// handle data events
 	req.on( "data",function( chunk ){
-		boddarr.push( chunk );
+		uploadedSize += chunk.length;
+		var bufferStore = file.write(chunk);
+		if(bufferStore == false)
+			req.pause();
 	});
+	
+	// handle drain events
+	file.on('drain', function() {
+	    req.resume();
+	});
+	
+	// handle end event
 	req.on("end",function(){
-		message = boddarr.join();
-		
-		getBlobService().createBlockBlobFromText("messages" , message_id, message, function(error){
+		// save data to blob service with message_id
+		getBlobService().createBlockBlobFromFile("messages" , message_id, tempFilePath, function(error){
 			if(!error){
 				console.log("message stored!");
 				res.send(200,[{ status:"OK"}]);
 			}else{
 				console.log("error",error);
 			}
+			// delete the temp file
+			fs.unlink(tempFilePath,function(err){
+
+			});
 		});
 	});
 }
 
-
-function setHostname(hostname)
+function createTempFilePath()
 {
-	server_hostname = hostname;
+	var tempFileName =  GUIDUtil.GUID();
+	return __dirname + "/temp/"+tempFileName;
 }
-
-
 
 
 
@@ -222,4 +228,3 @@ exports.submitMessageMetadata = submitMessageMetadata;
 exports.uploadMessage = uploadMessage;
 exports.getMessage = getMessage;
 exports.getUserMessages = getUserMessages;
-exports.setHostname = setHostname;
