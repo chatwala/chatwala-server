2e5ff4fcd90bc852f10eaba03423f429eb623be0
K <mustkre8@gmail.com>
2013-12-27 15:28:06 -0500

    returning 404 error code when user not found
    Signed-off-by: K <mustkre8@gmail.com>

--- routes/messages.js
+++ routes/messages.js
@@ -76,7 +76,7 @@ function getUserMessages( req, res )
 				var results = { "user":user_id ,"messages":messages};
 				res.send(200,results)
 			}else{
-				res.send(200,{"status":"user not found!"});
+				res.send(404,{"status":"user not found"});
 			}
 			db.close();
 		});
