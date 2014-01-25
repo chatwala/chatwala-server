7d2417dc99a39775ffd6b08f61d9b6e90797b7b4
Kevin Miller <Airswoop1@gmail.com>
2014-01-24 20:54:22 -0500

    added check on db for whether user has been assigned push_token

--- routes/users.js
+++ routes/users.js
@@ -21,22 +21,27 @@ function registerNewUserWithPush( req, res){
 			if(platform_type === 'ios'){
 				storePushCertToDB(user_id, push_token, function(err, user){
 					if(!err){
+						try{
+							var payload = {
+								alert: "Hello!"
+							}
 
-						var payload = {
-							alert: "Hello!"
+							var hub = azure.createNotificationHubService('chatwala-dev-push');
+							hub.apns.send(user.devices[0], payload, function(err){
+								if(err){
+									console.log("Error sending APNS payload to " + user_id);
+									console.log(err);
+								}
+								else{
+									console.log('hitting registerNewUserWithPush for ios client');
+									res.send(200,{"status":"OK"});
+								}
+							})
+						}catch(e){
+							console.log("error trying to connet to Service Bus")
+							console.log(e);
 						}
 
-						var hub = azure.createNotificationHubService('chatwala-dev-push');
-						hub.apns.send(user.devices[0], payload, function(err){
-							if(err){
-								console.log("Error sending APNS payload to " + user_id);
-								console.log(err);
-							}
-							else{
-								console.log('hitting registerNewUserWithPush for ios client');
-								res.send(200,{"status":"OK"});
-							}
-						})
 					}
 					else{
 						console.log("Error assigning user push_token");
@@ -68,13 +73,26 @@ function storePushCertToDB( user_id, token_id, callback){
 		} 
 		else {
 			var collection = db.collection('users');
-			collection.findAndModify({"user_id":user_id},{ $push:{"devices": token_id  }},{},function(err,object){
-				if(!err){
-					callback(null, object);
-				}
-				else{
+			collection.find({"user_id":user_id, "devices": token_id}, function(err, obj){
+				if(err){
 					callback(err);
 				}
+				else if(obj.length === 0){
+
+					collection.findAndModify({"user_id":user_id},{ $push:{"devices": token_id  }},{},function(err,object){
+						if(!err){
+							callback(null, object);
+						}
+						else{
+							callback(err);
+						}
+					})
+				}
+				else{
+					console.log("No need to update user's devices becauase token_id already exists");
+					callback(null);
+				}
+
 			})
 		}
 	});
@@ -118,8 +136,78 @@ Endpoint Handler for retrieving message file
 **/
 function getProfilePicture( req, res )
 {
-	var user_id = req.params.user_id;
-//create a SAS that expires in an hour
-var sharedAccessPolicy = {
-	AccessPolicy: {
-		Permissions: 'r',
+        var user_id = req.params.user_id;
+        
+        //create a SAS that expires in an hour
+        var sharedAccessPolicy = {
+                AccessPolicy: {
+                        Permissions: 'r',
+                        Expiry: azure.date.minutesFromNow(60)
+                }
+        };
+
+        var sasUrl = utility.getBlobService().getBlobUrl("pictures", user_id, sharedAccessPolicy);
+
+        if (sasUrl) {
+                console.log("Fetched shared access url for picture blob - redirecting");
+                res.writeHead(302, {
+                        'Location': sasUrl
+                });
+                res.end();
+        }
+        else {
+                console.log("Unable to retrieve shared access picture url for user: " + user_id);
+                res.send(404);
+        }
+}
+
+function updateProfilePicture( req, res )
+{
+        
+        // get user_id parameter
+        var user_id = req.params.user_id;
+        // create a temp file
+        var tempFilePath = utility.createTempFilePath();
+        var file = fs.createWriteStream(tempFilePath);
+
+        var fileSize = req.headers['content-length'];
+        var uploadedSize = 0;
+
+        // handle data events
+        req.on( "data",function( chunk ){
+                uploadedSize += chunk.length;
+                var bufferStore = file.write(chunk);
+                if(bufferStore == false)
+                        req.pause();
+        });
+        
+        // handle drain events
+        file.on('drain', function() {
+            req.resume();
+        });
+        
+        // handle end event
+        req.on("end",function(){
+                // save data to blob service with user_id
+                console.log("saving picture for userid", user_id)
+                //first agrument is the container it should prob be "profilePicture" instead of "messages"
+                utility.getBlobService().createBlockBlobFromFile("pictures" , user_id, tempFilePath, function(error){
+                        if(!error){
+                                console.log("profile picture stored!");
+                                res.send(200,[{ status:"OK"}]);
+                        }else{
+                                console.log("profile picture upload blob error",error);
+                                res.send(400,[{ error:"need image asset"}])
+                        }
+                        // delete the temp file
+                        fs.unlink(tempFilePath,function(err){
+
+                        });
+                });
+        });
+}
+
+exports.updateProfilePicture = updateProfilePicture;
+exports.getProfilePicture = getProfilePicture;
+exports.registerNewUser = registerNewUser;
+exports.registerNewUserWithPush = registerNewUserWithPush;
