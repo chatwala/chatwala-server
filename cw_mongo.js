116cf35d419e78b226552ea07fe956cf1c152f3b
rahulkumarny <rahul.kumar05@gmail.com>
2014-01-26 15:58:25 -0500

    Fixing cw_mongo

--- cw_mongo.js
+++ cw_mongo.js
@@ -5,35 +5,35 @@ var azure = null;
 var mongoDatabase;
 
 function getConnection(callback) {
-	
-	if (mongoDatabase) {
-		callback(null,mongoDatabase);
-	}
-	else {
-		console.log("Connecting to mongo database...");
-		
-		MongoClient.connect(mongo_url, function(err,db) {
-			if(err) {
-				console.log("Unable to connect to Mongo database.");
-				mongoDatabase = null;
-				callback(err, null);
-			}
-			else {
-				console.log("Connected to Mongo database.");
-				mongoDatabase = db;
-				callback(null, mongoDatabase);
-			}
-		});
-	}
+        
+        if (mongoDatabase) {
+                callback(null,mongoDatabase);
+        }
+        else {
+                console.log("Connecting to mongo database...");
+                
+                MongoClient.connect(mongo_url, function(err,db) {
+                        if(err) {
+                                console.log("Unable to connect to Mongo database.");
+                                mongoDatabase = null;
+                                callback(err, null);
+                        }
+                        else {
+                                console.log("Connected to Mongo database.");
+                                mongoDatabase = db;
+                                callback(null, mongoDatabase);
+                        }
+                });
+        }
 };
 
 function isConnected() {
-	if (mongoDatabase === undefined) {
-		return false;
-	}
-	else {
-		return true;
-	}
+        if (mongoDatabase === undefined) {
+                return false;
+        }
+        else {
+                return true;
+        }
 }
 
 module.exports.getConnection = getConnection;
