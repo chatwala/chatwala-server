94d9e0b5f824cde4ab4af6ae9b2d011f4c8984e5
samchatwala <sam@chatwala.com>
2014-01-28 20:23:24 -0500

    added 1.3 endpoints and cleaned up code

--- routes/messages.js
+++ routes/messages.js
@@ -4,233 +4,356 @@ var GUIDUtil = require('GUIDUtil');
 var os = require("os");
 var CWMongoClient = require('../cw_mongo.js');
 var config = require('../config.js')();
-var hub = azure.createNotificationHubService(config.azure.hub_name, config.azure.hub_endpoint,config.azure.hub_keyname,config.azure.hub_key);
+var hub = azure.createNotificationHubService(config.azure.hub_name, config.azure.hub_endpoint, config.azure.hub_keyname, config.azure.hub_key);
 
 var NO_FILES = "files not found";
 var NO_BODY = "no post information found for POST /messages";
 
 var utility = require('../utility');
 
-function compareMessageMetadata(a,b) {
-  if (a.timestamp < b.timestamp)
-     return 1;
-  if (a.timestamp > b.timestamp)
-    return -1;
-  return 0;
+function compareMessageMetadata(a, b) {
+    if (a.timestamp < b.timestamp)
+        return 1;
+    if (a.timestamp > b.timestamp)
+        return -1;
+    return 0;
 }
 
 /**
+ Returns List of User's Messages
+ **/
+function getUserMessages(req, res) {
+    var user_id = req.params.user_id;
 
-Returns List of User's Messages
+    CWMongoClient.getConnection(function (err, db) {
 
-**/
+        if (err) {
+            res.send(500, {"error": "unable to fetch messages - database error: " + err});
+        } else {
+            var collection = db.collection('users');
+            collection.findOne({"user_id": user_id}, function (err, user) {
 
-function getUserMessages( req, res ) {
-        var user_id = req.params.user_id;
-        
-        CWMongoClient.getConnection(function (err, db) {
-        
-                if (err) { 
-                        res.send(500,{"error":"unable to fetch messages - database error: " + err});
+                if (!err && user && user.inbox) {
+                    var messages = user.inbox.sort(compareMessageMetadata);
+                    console.log("user messages fetched for user: " + user_id);
+                    var results = { "user": user_id, "messages": messages};
+                    res.send(200, results)
                 } else {
-                        var collection = db.collection('users');
-                        collection.findOne({"user_id":user_id}, function(err,user){
-                        
-                                if(!err && user && user.inbox) {
-                                        var messages = user.inbox.sort(compareMessageMetadata);
-                                        console.log("user messages fetched for user: " + user_id);
-                                        var results = { "user":user_id ,"messages":messages};
-                                        res.send(200,results)
-                                } else{
-                                        console.log("unable to fetch message for user " + user_id + " error: ", err);
-                                        res.send(404);
-                                }                
-                        });        
+                    console.log("unable to fetch message for user " + user_id + " error: ", err);
+                    res.send(404);
                 }
-        });        
+            });
+        }
+    });
 }
 
 /**
-  Endpoint Handler for retrieving SAS Url for file upload
-**/
-function getSASurl( req, res )
-{
-  var message_id = req.params.message_id;
+ Endpoint Handler for saving message meta data and returning the url as well as the upload url
+ **/
+function postMessage(req, res) {
 
-  //create a SAS that expires in an hour
-  var sharedAccessPolicy = {
-    AccessPolicy: {
-      Permissions: 'rw',
-      Expiry: azure.date.minutesFromNow(10)
+    if (!req.hasOwnProperty('body')) {
+        console.log("Error on postMessage : no body");
+        res.send(400, [
+            { error: "need body"}
+        ]);
+        return;
     }
-  };
 
-  var sasUrl = utility.getBlobService().getBlobUrl("messages", message_id, sharedAccessPolicy);
+    var message_id = req.params.message_id;
+    var recipient_id = req.body.recipient_id;
+    var sender_id = req.body.sender_id;
+    var host = req.headers.host;
 
-  if (sasUrl) {
-    console.log("Fetched shared access message url for blob - redirecting");
-    res.send(200, {'url': sasUrl});
-  }
-  else {
-    console.log("Unable to retrieve shared access url for message: " + message_id);
-    res.send(404);
-  }
+    if (typeof message_id === 'undefined') {
+        res.send(400, [
+            { error: "need message_id"}
+        ]);
+        return;
+    }
+
+    if (typeof recipient_id === 'undefined') {
+        res.send(400, [
+            { error: "need recipient_id"}
+        ]);
+        return;
+    }
+
+    if (typeof sender_id === 'undefined') {
+        res.send(400, [
+            { error: "need sender_id"}
+        ]);
+        return;
+    }
+
+
+    storeMessageMetadataInDB(message_id, recipient_id, sender_id, host, function (err, url) {
+        if (err) {
+            res.send(500, {"status": "FAIL", "message": "could not store message metadata"});
+        }
+        else {
+            var sasUrl = getSasURL(message_id);
+
+            if (sasUrl) {
+                console.log("Fetched shared access message url for blob - redirecting");
+                res.send(200, {"status": "OK", 'url': url, 'sasUrl': sasUrl});
+            }
+            else {
+                console.log("Unable to retrieve shared access url for message: " + message_id);
+                res.send(500, {"status": "FAIL", "message": "Unable to create shared access url for message " + message_id});
+            }
+        }
+    });
+}
+
+function getUploadURL(req, res) {
+    var message_id = req.params.message_id;
+    var sasUrl = getSasURL(message_id);
+    if(sasUrl) {
+        res.send(200, {"status":"OK", "sasUrl": sasUrl});
+    }
+    else {
+        console.log("Unable to retrieve shared access url for message: " + message_id);
+        res.send(500, {"status": "FAIL", "message": "Unable to create shared access url for message " + message_id});
+    }
+}
+
+function getSasURL(message_id) {
+    //create a SAS that expires in 10 minutes
+    var sharedAccessPolicy = {
+        AccessPolicy: {
+            Permissions: 'rw',
+            Expiry: azure.date.minutesFromNow(10)
+        }
+    };
+
+    var sasUrl = utility.getBlobService().getBlobUrl("messages", message_id, sharedAccessPolicy);
+    return sasUrl;
+}
+
+function postFinalize(req, res) {
+
+    if (!req.hasOwnProperty('body')) {
+        console.log("Error on postFinalize : no body");
+        res.send(400, [
+            { error: "need body"}
+        ]);
+        return;
+    }
+
+    var message_id = req.params.message_id;
+    var recipient_id = req.body.recipient_id;
+
+    if (typeof message_id === 'undefined') {
+        res.send(400, [
+            { error: "need message_id"}
+        ]);
+        return;
+    }
+
+    if (typeof recipient_id === 'undefined') {
+        res.send(400, [
+            { error: "need recipient_id"}
+        ]);
+        return;
+    }
+
+    sendPushNotification(recipient_id, function (err) {
+        res.send(200, {"status": "OK", "message": "finalize successfully completed"});
+    });
+
 }
 
 /**
-        Endpoint Handler for retrieving message file
-**/
-function getMessage( req, res )
-{
-        var message_id = req.params.message_id;
+ Endpoint Handler for retrieving message file
+ **/
+function getMessage(req, res) {
 
-        //create a SAS that expires in an hour
-        var sharedAccessPolicy = {
-                AccessPolicy: {
-                        Permissions: 'r',
-                        Expiry: azure.date.minutesFromNow(60)
-                }
-        };
+    var message_id = req.params.message_id;
 
-        var sasUrl = utility.getBlobService().getBlobUrl("messages", message_id, sharedAccessPolicy);
+    if (typeof message_id === 'undefined') {
+        res.send(400, [
+            { error: "need message_id"}
+        ]);
+        return;
+    }
 
-        if (sasUrl) {
-                console.log("Fetched shared access message url for blob - redirecting");
-                res.writeHead(302, {
-                        'Location': sasUrl
-                });
-                res.end();
+    //create a SAS that expires in an hour
+    var sharedAccessPolicy = {
+        AccessPolicy: {
+            Permissions: 'r',
+            Expiry: azure.date.minutesFromNow(60)
         }
-        else {
-                console.log("Unable to retrieve shared access url for message: " + message_id);
-                res.send(404);
-        }
-}
+    };
 
+    var sasUrl = utility.getBlobService().getBlobUrl("messages", message_id, sharedAccessPolicy);
 
-function submitMessageMetadata( req, res )
-{
-        if(req.hasOwnProperty("body")) {
-        
-                var recipient_id = req.body.recipient_id;
-                var sender_id = req.body.sender_id;
-                
-                var message_metadata =  req.body;
-                message_metadata.message_id = GUIDUtil.GUID();
-                message_metadata.timestamp = Math.round((new Date()).getTime() / 1000);
-                message_metadata.thumbnail = "http://" + req.headers.host + "/users/" + sender_id + "/picture";
-                
-                saveOutGoingMessage(message_metadata, function(err) {
-                        if(err)throw err;
-                        var results = {status:"OK", message_id:message_metadata.message_id, url: ("http://chatwala.com/?" + message_metadata.message_id)};
-                        console.log("sending response: ", results);
-                        res.send(200, results);
-                });
-        }
-        else {
-                console.log("NO_BODY");
-        res.send(400,{status:"FAIL", message:"NO_BODY"});
-        }
-}
-
-function saveOutGoingMessage( message_metadata, callback ) {
-        CWMongoClient.getConnection(function (err, db) {
-        
-                if (err) { 
-                        res.send(500,{"error":"unable to fetch messages - database error: " + err});
-                }
-                else {
-                        var collection = db.collection('users');
-                        var sender_id = message_metadata.sender_id;
-                        var recipient_id = message_metadata.recipient_id;
-                
-                        console.log("sender: ", sender_id);
-                        console.log("locating recipient: ",recipient_id);
-                
-                        if(message_metadata.recipient_id == "unknown_recipient") {
-                                // unknown recipient
-                                callback(null);
-                        } else{
-                                // known recipient
-                                console.log("saving message: ", message_metadata );
-                        
-                                collection.findAndModify({"user_id":recipient_id},[['_id','asc']],{ $push:{"inbox": message_metadata  }},{},function(err,object){
-                                        if(!err) {
-                                                console.log("updated inbox for recipient: " + recipient_id);
-                                                var payload = {"content_available": 1,"message": "You have a received a new Chatwala reply."};
-
-                                                hub.send(recipient_id, payload, function(err,result,responseObject){
-                                                        if(err){
-                                                                console.log("Error sending APNS payload to " + recipient_id);
-                                                                console.log(err);
-                                                                callback(err);
-                                                        }
-                                                        else{
-                                                                console.log('successfully sent push notification to user: ' + recipient_id);
-                                                                callback(null);
-                                                        }
-                                                })
-
-                                        } 
-                                        else{
-                                                callback("unable to save outbound message - cannot find recipient: ", recipient_id);
-                                        }
-                                });
-                        }
-                }
+    if (sasUrl) {
+        console.log("Fetched shared access message url for blob - redirecting");
+        res.writeHead(302, {
+            'Location': sasUrl
         });
+        res.end();
+    }
+    else {
+        console.log("Unable to retrieve shared access url for message: " + message_id);
+        res.send(404);
+    }
+}
+
+
+function storeMessageMetadataInDB(message_id, recipient_id, sender_id, host, callback) {
+    var message_metadata = {
+        "message_id": message_id,
+        "recipient_id": recipient_id,
+        "sender_id": sender_id,
+        "timestamp": Math.round((new Date()).getTime() / 1000),
+        "thumbnail": "http://" + host + "/users/" + sender_id + "/picture"
+
+    };
+
+    saveOutGoingMessage(message_metadata, function (err) {
+        callback(err, ("http://" + host +"/messages/" + message_metadata.message_id));
+    });
+}
+
+/*Deprecated*/
+function submitMessageMetadata(req, res) {
+    if (req.hasOwnProperty("body")) {
+
+        var recipient_id = req.body.recipient_id;
+        var sender_id = req.body.sender_id;
+        var message_id = GUIDUtil.GUID();
+
+        storeMessageMetadataInDB(message_id, recipient_id, sender_id, req.headers.host, function (err, url) {
+            if (err) {
+                res.send(500, {"status": "FAIL", "message": "could not store message data"});
+            }
+            else {
+                console.log("sending response: ", url);
+                res.send(200, {"status": "OK", "message_id": message_id, "url": url});
+            }
+        });
+
+    }
+    else {
+        console.log("NO_BODY");
+        res.send(400, {status: "FAIL", message: "NO_BODY"});
+    }
+}
+
+function saveOutGoingMessage(message_metadata, callback) {
+    CWMongoClient.getConnection(function (err, db) {
+        if (err) {
+            res.send(500, {"error": "unable to fetch messages - database error: " + err});
+        }
+        else {
+            var collection = db.collection('users');
+            var sender_id = message_metadata.sender_id;
+            var recipient_id = message_metadata.recipient_id;
+
+            console.log("sender: ", sender_id);
+            console.log("locating recipient: ", recipient_id);
+
+            if (message_metadata.recipient_id == "unknown_recipient") {
+                callback(null);
+            }
+            else {
+                console.log("saving message: ", message_metadata);
+                collection.update(
+                    { "user_id": recipient_id},
+                    { $push: {"inbox": message_metadata  }},
+                    { "upsert": true },
+                    function (err, object) {
+                        if (!err) {
+                            console.log("updated inbox for recipient: " + recipient_id);
+                            callback(null);
+                        }
+                        else {
+                            callback("unable to save outbound message - cannot find recipient: ", recipient_id);
+                        }
+                    }
+                );
+            }
+        }
+    });
+}
+
+function sendPushNotification(recipient_id, callback) {
+    var payload = {"content_available": 1, "message": "You have a received a new Chatwala reply."};
+
+    hub.send(recipient_id, payload, function (err, result, responseObject) {
+        if (err) {
+            console.log("Error sending APNS payload to " + recipient_id);
+            console.log(err);
+            callback(err);
+        }
+        else {
+            console.log('successfully sent push notification to user: ' + recipient_id);
+            callback(null);
+        }
+    });
 }
 
 /**
  Endpoint Handler for Chatwala File (PUT)
-**/
-function uploadMessage( req, res ) {
+ **/
+function putMessage(req, res) {
+    // get message_id parameter
+    var message_id = req.params.message_id
 
-        // get message_id parameter
-        var message_id = req.params.message_id;
-        // create a temp file
-        var tempFilePath = utility.createTempFilePath();
-        var file = fs.createWriteStream(tempFilePath);
-        
+    if (typeof message_id === 'undefined') {
+        res.send(400, [
+            { error: "need message_id"}
+        ]);
+        return;
+    }
 
-        var fileSize = req.headers['content-length'];
-        var uploadedSize = 0;
-        
-        console.log("storing message blob with ID:",message_id);
+    // create a temp file
+    var tempFilePath = utility.createTempFilePath();
+    var file = fs.createWriteStream(tempFilePath);
+    var fileSize = req.headers['content-length'];
+    var uploadedSize = 0;
 
-        // handle data events
-        req.on( "data",function( chunk ){
-                uploadedSize += chunk.length;
-                var bufferStore = file.write(chunk);
-                if(bufferStore == false)
-                        req.pause();
+    console.log("storing message blob with ID:", message_id);
+
+    // handle data events
+    req.on("data", function (chunk) {
+        uploadedSize += chunk.length;
+        var bufferStore = file.write(chunk);
+        if (bufferStore == false)
+            req.pause();
+    });
+
+    // handle drain events
+    file.on('drain', function () {
+        req.resume();
+    });
+
+    // handle end event
+    req.on("end", function () {
+        // save data to blob service with message_id
+        utility.getBlobService().createBlockBlobFromFile("messages", message_id, tempFilePath, function (error) {
+            if (!error) {
+                console.log("message stored!");
+                res.send(200, [
+                    { status: "OK"}
+                ]);
+            } else {
+                console.log("error", error);
+            }
+            // delete the temp file
+            fs.unlink(tempFilePath, function (err) {
+
+            });
         });
-        
-        // handle drain events
-        file.on('drain', function() {
-            req.resume();
-        });
-        
-        // handle end event
-        req.on("end",function(){
-                // save data to blob service with message_id
-                utility.getBlobService().createBlockBlobFromFile("messages" , message_id, tempFilePath, function(error){
-                        if(!error){
-                                console.log("message stored!");
-                                res.send(200,[{ status:"OK"}]);
-                        }else{
-                                console.log("error",error);
-                        }
-                        // delete the temp file
-                        fs.unlink(tempFilePath,function(err){
-
-                        });
-                });
-        });
+    });
 }
 
 exports.submitMessageMetadata = submitMessageMetadata;
-exports.uploadMessage = uploadMessage;
-exports.getMessage = getMessage;
+exports.putMessage = putMessage;
+exports.getMessage = getMessage
+exports.postMessage = postMessage;
 exports.getUserMessages = getUserMessages;
-exports.getSASurl = getSASurl;
+exports.getUploadURL = getUploadURL;
+exports.postFinalize = postFinalize;
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-
 /**
  * Module dependencies.
  */
- 
-console.log("Initializing node: " + new Date()); 
+
+console.log("Initializing node: " + new Date());
 var config = require('./config.js')();
 
 "use strict";
@@ -24,19 +23,19 @@ var queue = [];
 // all environments
 
 mongoClient.getConnection(function (err, db) {
-        if (err) {
-                console.log("Unable to connect to mongo DB."); 
-                queue.forEach(function (object) {
-                        object.res.send(500);
-                });
-        } else {
-                console.log("Launching queued requests"); 
-                queue.forEach(function (object) {
-                        object.next();
-                });
-        }
-        
-        queue = [];
+    if (err) {
+        console.log("Unable to connect to mongo DB.");
+        queue.forEach(function (object) {
+            object.res.send(500);
+        });
+    } else {
+        console.log("Launching queued requests");
+        queue.forEach(function (object) {
+            object.next();
+        });
+    }
+
+    queue = [];
 });
 
 app.set('port', process.env.PORT || 1337);
@@ -50,51 +49,51 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 
 app.use(function (req, res, next) {
-        if (mongoClient.isConnected()) { 
-                next(); 
-        }
-        else {
-                console.log("Database not connected yet, queuing request");
-                   queue.push({ req : req, res : res, next : next});
-        }
+    if (mongoClient.isConnected()) {
+        next();
+    }
+    else {
+        console.log("Database not connected yet, queuing request");
+        queue.push({ req: req, res: res, next: next});
+    }
 });
 
 app.use(function (req, res, next) {
-        var authHeaderValue = "";
-        var idHeaderValue = "";
-        
-        if (req.url === "/monitor") {
-                // The one exception to the authorization logic
-                next();
-                return;
-        }
-        
-        for(var item in req.headers) {
-        
-                if (item == "x-chatwala") {
-                        // Validate                        
-                        authHeaderValue = req.headers[item];
-                }
-                else if (item == "x-id") {
-                        idHeaderValue = req.headers[item];
-                }
-          }
-        
-        if (!authHeaderValue || authHeaderValue === "") {
-                console.log ("Authorization header is empty or not found");
-                res.send(401, {error:"Not Authorized: missing headers"});
-                return;
-        }
-        else {
-                var expectedToken = clientID + ":" + clientSecret;
-                
-                if (expectedToken != authHeaderValue) {
-                        res.send(401, {error:"Not Authorized"});
-                        return;
-                }
-        }
+    var authHeaderValue = "";
+    var idHeaderValue = "";
 
+    if (req.url === "/monitor") {
+        // The one exception to the authorization logic
         next();
+        return;
+    }
+
+    for (var item in req.headers) {
+
+        if (item == "x-chatwala") {
+            // Validate
+            authHeaderValue = req.headers[item];
+        }
+        else if (item == "x-id") {
+            idHeaderValue = req.headers[item];
+        }
+    }
+
+    if (!authHeaderValue || authHeaderValue === "") {
+        console.log("Authorization header is empty or not found");
+        res.send(401, {error: "Not Authorized: missing headers"});
+        return;
+    }
+    else {
+        var expectedToken = clientID + ":" + clientSecret;
+
+        if (expectedToken != authHeaderValue) {
+            res.send(401, {error: "Not Authorized"});
+            return;
+        }
+    }
+
+    next();
 });
 
 app.use(app.router);
@@ -104,35 +103,60 @@ app.use(express.multipart());
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 
 // routing
-app.get('/monitor', function(req, res) {
+app.get('/monitor', function (req, res) {
 
-	// Always return success - used for monitoring
-	res.send(200);
+    // Always return success - used for monitoring
+    res.send(200);
 
 });
 
 app.get('/', routes.index);
+
+//Deprecated
 app.get('/register', users.registerNewUser);
-app.post('/register', users.registerNewUserWithPush);
-app.get('/users/:user_id/messages', messages.getUserMessages );
-//app.get('/users/:user_id/messages/:message_id', messages.getMessage );
-app.get('/messages/:message_id', messages.getMessage );
-app.post('/messages/:message_id', messages.getSASurl)
-app.post('/messages', messages.submitMessageMetadata );
-app.put('/messages/:message_id', messages.uploadMessage);
-app.get('/users/:user_id/picture', users.getProfilePicture)
-app.put('/users/:user_id/picture', users.updateProfilePicture)
+
+//Registers a push token for a user
+app.post('/registerPushToken', users.postPushToken);
+
+app.get('/users/:user_id/messages', messages.getUserMessages);
+
+//302 redirects to SASURL
+app.get('/messages/:message_id', messages.getMessage);
+
+//posts metadata and returns SASURL
+app.post('/messages/:message_id', messages.postMessage);
+
+//posts finalize on message
+app.post('/messages/:message_id/finalize', messages.postFinalize);
+
+//refresh PUT SASURL
+app.get('/messages/:message_id/uploadURL', messages.getUploadURL);
+
+//deprecated, old put message call, put directly to storage instead
+app.put('/messages/:message_id', messages.putMessage);
+
+//deprecated
+app.post('/messages', messages.submitMessageMetadata);
+
+//302 redirects to SASURL
+app.get('/users/:user_id/picture', users.getProfilePicture);
+
+//deprecated
+app.put('/users/:user_id/picture', users.updateProfilePicture);
+
+//get SASURL
+app.get('/users/:user_id/pictureUploadURL', users.getProfilePictureUploadURL);
 
 var server = http.createServer(app);
 
-server.listen(app.get('port'), function(){
-  console.log('listening on port ' + app.get('port') + " started:  ");
-  console.log("Completed Node initialization: " + new Date());
+server.listen(app.get('port'), function () {
+    console.log('listening on port ' + app.get('port') + " started:  ");
+    console.log("Completed Node initialization: " + new Date());
 });
 // var serveraddress = server.address();
 // console.log("serveraddress",serveraddress);
