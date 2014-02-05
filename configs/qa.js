99e6c4b6ff239be052f4e3d70ea9e4966f521da4
Kevin Miller <Airswoop1@gmail.com>
2014-02-04 22:33:13 -0500

    added qa config

---
+++ configs/qa.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "db": {
+        "mongodb": "mongodb://chatwala-dev:OFXd6OICqM8vr.ME3OCbh.l2T9GjAuXFF.7Hp9gDXNE-@ds030607.mongolab.com:30607/chatwala-dev"
+    },
+    "azure": { 
+        "storage_name": "chatwalaqa",
+        "storage_key": "7XPBy0ojEUrW+AZAi2YRGs26UAR4c/f+zLwHZbvsJIrx7LP9KQ1pV4xjvYyOVSIjCnTJ6bO2qld9BzrXBa51Cg==",
+        "partition_key": "chatwalaqa",
+        "hub_name" : "chatwala-qa-push",
+        "hub_endpoint" : "sb://chatwala-dev-push-ns.servicebus.windows.net/",
+        "hub_keyname" : "DefaultFullSharedAccessSignature",
+        "hub_key" : "gePyfv/kICC7LLYzvoZHmjO4yXZQoED1xUkjb5mHqsQ="
+    }
+};
