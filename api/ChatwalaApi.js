b83ac604d5d21f2d1b4087db4900c92d2d51d23b
samchatwala <sam@chatwala.com>
2014-02-14 15:35:25 -0500

    2.0

--- api/ChatwalaApi.js
+++ api/ChatwalaApi.js
@@ -1,13 +1,8 @@
 var UnkownRecipientMessageSend = require("UnknownRecipientMessageSend");
 
 var ChatwalaApi = {
-
-    requests: {
-        "unknownRecipientMessageSendRequest": UnkownRecipientMessageSend.cwRequest
-    },
-
-    responses: {
-        "unknownRecipientMessageSendResponse": UnkownRecipientMessageSend.cwResponse
+    commands: {
+        "UnkownRecipientMessageSend":UnkownRecipientMessageSend.$
     }
 };
 exports.$ = ChatwalaApi;
