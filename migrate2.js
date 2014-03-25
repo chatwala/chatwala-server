450619f7f35f1724884602b03a7c6a689f46c694
Kevin Miller <Airswoop1@gmail.com>
2014-03-25 11:30:23 -0400

    modified migrate to count number of migrated blobs

--- migrate2.js
+++ migrate2.js
@@ -14,6 +14,7 @@ var GUIDUtil = require('GUIDUtil');
 var rimraf = require('rimraf');
 var AdmZip = require('adm-zip');
 var archiver = require('archiver');
+var request = require('request');
 var MigrateHelper = require('./api/migrate/MigrateHelper.js');
 
 "use strict";
@@ -51,6 +52,84 @@ function migrate() {
     MigrateHelper.migrateAllWalas();
 }
 
-migrate();
+function checkWalasForUserId(user_id){
+    CWMongoClient.getConnection(function (err, db) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            var collection = db.collection('messages');
+            var query = {};
+            query[ChatwalaMessageDocuments.MESSAGE_PROPERTIES.OWNER_USER_ID] = user_id;
+            query[ChatwalaMessageDocuments.MESSAGE_PROPERTIES.OWNER_ROLE] = ChatwalaMessageDocuments.ROLE_RECIPIENT;
 
+            collection.find(
+                query,
+                {},
+                function(err, cursor) {
+                    if(err) {
+                        var response = new Response();
+                        response.response_code = responseCodes["failure"];
+                        callback(err, response);
+                    }
+                    else {
+                        cursor.toArray(function(err, documents) {
+                            console.log(err);
+
+                            if(documents){
+                                console.log("documents: ");
+                                console.log(documents);
+                                console.log(documents.length);
+                                var message_id_array = [];
+
+                                for(var i=0; i<documents.length;i++) {
+
+                                    if(message_id_array.indexOf(documents[i].message_id) < 0){
+                                        message_id_array.push(documents[i].message_id);
+                                    }
+                                }
+                                var doesnt_exist = [];
+
+                                checkExists(message_id_array,0,doesnt_exist, function(does_not_exist){
+                                   console.log("The following message_id's do not exist on blob storage: ");
+                                   console.log(does_not_exist);
+                                })
+
+                            }
+                            else{
+                                console.log("no documents from query");
+                                console.log(err);
+                            }
+                        });
+
+                    }
+                }
+            )
+        }
+    });
+
+}
+
+function checkExists(messages_array, index, notExist, callback){
+    if(index === messages_array.length){
+        callback(notExist);
+    }
+    var msg_id = messages_array[index];
+    var url = "http://chatwalaprodshard1.blob.core.windows.net/messages/" + msg_id;
+    console.log(url);
+    request(url, function(error, response, body){
+        console.log(response.statusCode);
+        if (response.statusCode !== 200) {
+            notExist.push({
+                "code": response.statusCode,
+                "message_id": msg_id
+            })
+        }
+        checkExists(messages_array,++index,notExist,callback);
+    })
+
+}
+
+//migrate();
+checkWalasForUserId("d80ba84b-7785-c219-5ee5-74130eff8e2a");
 
